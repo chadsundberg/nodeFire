0eb2100e16ea6bcd91aca531dd9cd2ebec286ad6
Chad Sundberg <chadsundberg@gmail.com>
2017-03-28 17:40:52 -0500

    review table updating and form clearing

--- public/scripts/controllers/review.controller.js
+++ public/scripts/controllers/review.controller.js
@@ -4,9 +4,18 @@ function($routeParams, CardDetailFactory) {
   console.log($routeParams);
   var self = this;
   self.placeDetails = CardDetailFactory.allDetails;
+  self.newReview = CardDetailFactory.newReview;
 
+
+// console.log('self.newReview', self.newReview);
   CardDetailFactory.getPlace($routeParams.placeId);
 
+  self.addNewReview = function() {
+    console.log('clicked', self.newReview);
+    CardDetailFactory.addNewReview(self.newReview);
+    self.newReview = {};
+
+  }
 
   // console.log(self.placeDetails);
 }]);
--- public/scripts/factories/carddetail.factory.js
+++ public/scripts/factories/carddetail.factory.js
@@ -1,6 +1,7 @@
 app.factory('CardDetailFactory', ['$firebaseAuth', '$http', function($firebaseAuth, $http) {
   console.log('card detail factory loaded');
   var placeDetails = { list: [] };
+  var reviewDetails = { list: {} };
   var auth = $firebaseAuth();
   console.log(placeDetails);
 
@@ -32,9 +33,39 @@ app.factory('CardDetailFactory', ['$firebaseAuth', '$http', function($firebaseAu
     });
   }
 
+  function addNewReview(newReview) {
+    console.log('factory getting place:', newReview);
+    var firebaseUser = auth.$getAuth();
+    // auth.$onAuthStateChanged(function(firebaseUser){
+      // firebaseUser will be null if not logged in
+      if(firebaseUser) {
+        // This is where we make our call to our server
+        firebaseUser.getToken().then(function(idToken){
+          $http({
+            method: 'POST',
+            url: '/cardDetail',
+            headers: {
+              id_token: idToken
+            },
+            data: newReview
+          }).then(function(response) {
+            console.log(response.data);
+            reviewDetails.list = response.data;
+          });
+        });
+      } else {
+        console.log('Not logged in or not authorized.');
+        self.secretData = "Log in to search for date activities.";
+      }
+
+
+  }
+
   return {
     allDetails: placeDetails,
-    getPlace: getPlace
+    getPlace: getPlace,
+    addNewReview: addNewReview,
+    newReview: reviewDetails
   }
 
   }]);
